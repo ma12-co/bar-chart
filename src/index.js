f3073c0f0895181b0cf85695eb3c714632a5c875
mrc <marcoambrosini@pm.me>
2019-04-02 15:02:34 +0200

    axes and some styling

--- src/index.js
+++ src/index.js
@@ -4,21 +4,24 @@ import "./index.css"
 import * as d3 from "d3"
 
 const dataset = require('./GDP-data.json')
-const data = dataset.data
+const DATA = dataset.data
 
-const WIDTH = 1200
-const HEIGHT = 600
+const WIDTH = 600
+const HEIGHT = 400
+const PADDING = 30
+const PADDINGLEFT = 50
+const PADDINGRIGHT = 40
+
 
 const yScale = d3.scaleLinear()
-  .domain([0, d3.max(data, (d) => d[1])])
-  .range([0, HEIGHT])
+  .domain([0, d3.max(DATA, (d) => d[1])])
+  .range([0, HEIGHT - 2 * PADDING])
 
+const xScale = d3.scaleLinear()
+  .domain([0, DATA.length])
+  .range([0, WIDTH - PADDINGLEFT - PADDINGRIGHT])
 
-
-
-
-
-let svg = d3.select("body")
+let svg = d3.select("div")
   .append('svg')
   .attr('width', WIDTH)
   .attr('height', HEIGHT)
@@ -26,23 +29,45 @@ let svg = d3.select("body")
 
 
 svg.selectAll("rect")
-  .data(data)
+  .data(DATA)
   .enter()
   .append("rect")
-  .attr("x", (d, i) => i * 3)
-  .attr("y", (d, i) => HEIGHT - yScale(d[1]))
-  .attr("width", 2)
+  .attr("x", (d, i) => xScale(i) + PADDINGLEFT)
+  .attr("y", (d, i) => HEIGHT - yScale(d[1]) - PADDING)
+  .attr("width", 1)
   .attr("height", (d) => yScale(d[1]))
   .attr("class", "bar")
+  .attr("data-date", d => d[0])
+  .attr("data-gdp", d => d[1])
 
-svg.selectAll('text')
-  .data(data)
-  .enter()
-  .append("text")
-  .text(d => d[0])
-  .attr("x", (d, i) => i * 3)
-  .attr("y", (d, i) => HEIGHT)
-  .attr('class', 'label')
-  .attr("transform", "translate(x,y) rotate(45)")
+const yAxisScale = d3.scaleLinear()
+  .domain([0, d3.max(DATA, (d) => d[1])])
+  .range([HEIGHT - 2 * PADDING, 0])
 
 
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yAxisScale);
+
+
+svg.append("g")
+  .attr("id", "x-axis")
+  .attr("transform", `translate(${PADDINGLEFT},${HEIGHT - PADDING})`)
+  .call(xAxis);
+
+
+svg.append("g")
+  .attr("id", "y-axis")
+  .attr("transform", `translate(${PADDINGLEFT},${PADDING}) `)
+  .call(yAxis);
+
+/**
+ svg.selectAll('text')
+.data(DATA)
+.enter()
+.append("text")
+.text(d => d[0])
+.attr("x", (d, i) => xScale(i) + PADDING)
+.attr("y", HEIGHT - PADDING + 10)
+.attr('class', 'label')
+
+ */
